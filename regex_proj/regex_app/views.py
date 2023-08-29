799015c7985765a9178e902fc49ca9928d0a8a53
mahak <mahakbabel26082003@mail.com>
2023-08-29 11:59:11 +0530

    added

---
+++ regex_proj/regex_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index (request):
+    
+    return render(request,'index.html')
+
+
+def positional(request):
+    
+    return render(request,'positional.html')
+
+def home(request):
+    
+    return render(request,'home.html')
+
+def catering(request):
+    
+    return render(request,'catering.html')
+
+
